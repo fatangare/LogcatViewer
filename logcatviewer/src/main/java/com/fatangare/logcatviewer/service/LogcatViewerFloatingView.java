163a3bbc9dd82329e01ccf1411281e071d2837cc
fuyuguang <2858311356@qq.com>
2016-12-10 17:47:43 +0800

    Fixed an bug, when deleting selected files
    Fixed an bug, when deleting selected files
    China:修复了一个bug , 当删除选中文件的时候

--- logcatviewer/src/main/java/com/fatangare/logcatviewer/service/LogcatViewerFloatingView.java
+++ logcatviewer/src/main/java/com/fatangare/logcatviewer/service/LogcatViewerFloatingView.java
@@ -505,7 +505,7 @@ public class LogcatViewerFloatingView extends StandOutWindow {
      */
     private void shareRecordedLogFiles() {
         //If none is selected, return
-        if (mListView.getCheckedItemCount() == 0) {
+        if (mRecordsListView.getCheckedItemCount() == 0) {
             Toast.makeText(getApplicationContext(), "First select log entry!", Toast.LENGTH_LONG).show();
             return;
         }
